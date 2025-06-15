a67f64e2e6d8fb1a5c8f17016efe918046eff404
neerCodes <kumarguptaneeraj100@gmail.com>
2025-06-15 16:51:08 +0530

    contact and footer

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import { HiH1 } from 'react-icons/hi2'
+import React from "react";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { HiH1 } from "react-icons/hi2";
 
 const Contact = () => {
-  return (
-    <h1>Contact</h1>
-  )
-}
 
-export default Contact
+  const handleFormSubmit = (formData) => {
+    console.log(formData.entries());
+    const formInputData = Object.fromEntries(formData.entries());
+    console.log('Formdata: ', formInputData)
+  }
+
+  return (
+    <section className="section-contact">
+      <h2 className="container-title">Contact Us</h2>
+
+      <div className="container contact-wrapper">
+        <form action={handleFormSubmit}>
+          <input
+            type="text"
+            className="form-control"
+            required
+            autoComplete="false"
+            placeholder="Enter your name"
+            name="username"
+          />
+          <input
+            type="email"
+            className="form-control"
+            required
+            autoComplete="false"
+            placeholder="Enter your email"
+            name="email"
+          />
+          <textarea
+            className="form-control"
+            rows="10"
+            placeholder="Enter your message"
+            name="message"
+            required
+            autoComplete="false"
+          ></textarea>
+          <button type="submit" value="send">
+            Send    <FaLongArrowAltRight />
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
