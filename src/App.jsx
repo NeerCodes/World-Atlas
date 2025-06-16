1ff72a8f0bca28c3e6e22ac99b6a29a4e93b6937
neerCodes <kumarguptaneeraj100@gmail.com>
2025-06-16 22:52:04 +0530

    Individual country details page and routing added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Contact from "./pages/Contact";
 import Country from "./pages/Country";
 import AppLayout from "./components/Layout/AppLayout";
 import ErrorPage from "./pages/ErrorPage";
+import CountryDetails from "./components/Layout/CountryDetails";
 
 const router = createBrowserRouter([
   {
@@ -29,6 +30,10 @@ const router = createBrowserRouter([
       {
         path: "country",
         element: <Country />,
+      },
+      {
+        path: "country/:id",
+        element: <CountryDetails />,
       },
     ],
   },
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState, useTransition } from "react";
+import { getCountryData } from "../api/postApi";
+import Loader from "../components/UI/Loader";
+import CountryCard from "../components/Layout/CountryCard";
 
 const Country = () => {
-  return (
-    <h1>Country</h1>
-  )
-}
+  const [isPending, startTransition] = useTransition();
+  const [countries, setCountries] = useState([]);
 
-export default Country
+  useEffect(() => {
+    startTransition(async () => {
+      const res = await getCountryData();
+      setCountries(res.data)
+    });
+  }, []);
+
+  if(isPending) return <Loader />
+  
+  return(
+    <section className="country-section">
+      <ul className="grid grid-four-cols">
+        {
+          countries.map((curCountry, index) => {
+            return <CountryCard country={curCountry} key={index} />
+          })
+        }
+      </ul>
+    </section>
+  )
+};
+
+export default Country;
